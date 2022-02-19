a9cb56877419351b2d1414cdf29910955935bf40
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-19 21:59:54 +0300

    ref: refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/app/weather_app.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
+  runApp(const WeatherApp());
 }
