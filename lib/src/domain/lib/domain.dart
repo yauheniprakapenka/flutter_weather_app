e462571bdaad2ede14381fdc6cbcd05c7c861d9f
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-22 13:30:48 +0300

    fix: fix sharing weather

--- lib/src/domain/lib/domain.dart
+++ lib/src/domain/lib/domain.dart
@@ -5,3 +5,4 @@ export 'src/providers/weather_provider/weather_bloc.dart';
 export 'src/providers/weather_provider/weather_state.dart';
 export 'src/repositories/i_weather_repository.dart';
 export 'src/use_cases/get_today_weather_use_case.dart';
+export 'src/use_cases/share_text_use_case.dart';
