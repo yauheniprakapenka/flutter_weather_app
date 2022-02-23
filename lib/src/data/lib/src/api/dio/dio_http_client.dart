4380db5fd5a4411d560afe07998792dc4dd265d0
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-23 16:11:13 +0300

    ref: refactor dio client

--- lib/src/data/lib/src/api/dio/dio_http_client.dart
+++ lib/src/data/lib/src/api/dio/dio_http_client.dart
@@ -2,10 +2,10 @@ import 'package:dio/dio.dart';
 
 import 'dio_logging_interceptor.dart';
 
-class DioClient {
-  final dio = Dio(
+Dio dioHttpClient(String baseUrl) {
+  return Dio(
     BaseOptions(
-      baseUrl: 'https://api.openweathermap.org/data/2.5/',
+      baseUrl: baseUrl,
       connectTimeout: 15000,
       receiveTimeout: 15000,
     ),
--- lib/src/data/lib/src/services/openweathermap_service.dart
+++ lib/src/data/lib/src/services/openweathermap_service.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_weather_app/src/domain/lib/domain.dart';
-import '../api/dio/dio_client.dart';
+import '../api/dio/dio_http_client.dart';
 import '../dto/dto.dart';
 
 class OpenweathermapService {
-  final _dio = DioClient().dio;
+  final _client = dioHttpClient('https://api.openweathermap.org/data/2.5/');
 
   /// Secret key
   static const _apiKey = '';
@@ -11,14 +11,14 @@ class OpenweathermapService {
   /// Access current weather data for any location.
   Future<WeatherDto> getCurrentWeather(Coordinates coordinates) async {
     final path = 'weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=$_apiKey';
-    final response = await _dio.get(path);
+    final response = await _client.get(path);
     return WeatherDto.fromJson(response.data);
   }
 
   /// Access current weather data for any location.
   Future<ForecastDto> getFiveDaysWeatherForecast(Coordinates coordinates) async {
     final path = 'forecast?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=$_apiKey';
-    final response = await _dio.get(path);
+    final response = await _client.get(path);
     return ForecastDto.fromJson(response.data);
   }
 }
