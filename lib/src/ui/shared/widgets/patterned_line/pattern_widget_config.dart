519d6de8b11d1a783279b82a0e6b8799bf860821
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-23 16:11:52 +0300

    fix: change pattern widget size

--- lib/src/ui/shared/widgets/patterned_line/pattern_widget_config.dart
+++ lib/src/ui/shared/widgets/patterned_line/pattern_widget_config.dart
@@ -1,5 +1,5 @@
 class PatternWidgetConfig {
-  static const containerSize = 3.0;
+  static const containerSize = 2.0;
   static const widthSize = containerSize * 3;
   static const heigthSize = containerSize * 2;
   static const paddingBetweenPatterns = 3;
